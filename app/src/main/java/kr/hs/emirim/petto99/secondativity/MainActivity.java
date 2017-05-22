66a6786aa42da15265aed6e57d76f64d2a86248b
kimyejin <kimyejintobi@gmail.com>
2017-05-22 20:50:46 +0900

    #2 override

--- app/src/main/java/kr/hs/emirim/petto99/secondativity/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/petto99/secondativity/MainActivity.java
@@ -2,6 +2,8 @@ package kr.hs.emirim.petto99.secondativity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater=getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
+
+        return true;
     }
 }
