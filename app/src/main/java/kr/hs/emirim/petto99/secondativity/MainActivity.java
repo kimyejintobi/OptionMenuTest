83e6fbc5095a4d146ea5fe34ca8cb693befc35d6
kimyejin <kimyejintobi@gmail.com>
2017-05-23 19:58:15 +0900

    mingming

--- app/src/main/java/kr/hs/emirim/petto99/secondativity/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/petto99/secondativity/MainActivity.java
@@ -1,16 +1,25 @@
 package kr.hs.emirim.petto99.secondativity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    LinearLayout linear;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear=(LinearLayout)findViewById(R.id.Linear);
+        but=(Button)findViewById(R.id.but_ok);
     }
 
     @Override
@@ -21,4 +30,29 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_yellow:
+                linear.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.item_but_rotate:
+                but.setRotation(but.getRotation()+45);
+                return true;
+            case R.id.item_but_size:
+                but.setScaleX(but.getScaleX()+2);
+                return true;
+
+        }
+        return false;
+    }
 }
+
